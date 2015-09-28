08e5a9abb540c73edd1a646cc71e2b907037e116
Gibran Hemani <explodecomputer@gmail.com>
2015-09-28 15:13:31 +0100

    updated

--- 03-snpdata.sh
+++ 03-snpdata.sh
@@ -7,11 +7,20 @@ source config
 # Provision for having dosage data and convert to best guess if the analyst doesn't have this
 
 # Copy raw data to modifiable data
+
+echo "Copying genetic data to processing folder"
 cp ${bfile_raw}.bed ${bfile}.bed
 cp ${bfile_raw}.bim ${bfile}.bim
 cp ${bfile_raw}.fam ${bfile}.fam
 
+# Change SNP ids to chr:position:{SNP/INDEL}
+echo "Updating SNP ID coding"
+cp ${bfile}.bim ${bfile}.bim.original
+awk '{if (length($5) == "1" && length($6) == "1") print $1, $1":"$4":SNP", $3, $4, $5, $6;else print $1, $1":"$4":INDEL", $3, $4, $5, $6;}' ${bfile}.bim.original > ${bfile}.bim
+
+
 # Make GRMs and calculate PCs
+echo "Creating kinship matrix and calculating PCs"
 gunzip -c ${hm3_snps} > temp_hm3snps.txt
 ${plink} \
 	--bfile ${bfile} \
@@ -23,9 +32,11 @@ ${plink} \
 rm temp_hm3snps.txt
 
 # Get genetic outliers
+echo "Detecting genetic outliers"
 R --no-save --args ${pcs_all} ${pca_sd} ${n_pcs} ${genetic_outlier_ids} < resources/genetics/genetic_outliers.R
 
 # Remove genetic outliers from data
+echo "Removing genetic outliers"
 ${plink} \
 	--bfile ${bfile} \
 	--remove ${genetic_outlier_ids} \
@@ -41,9 +52,11 @@ ${gcta64} \
 # Create pedigree matrix if family data, otherwise remove related individuals from existing kinship and data file
 if [ "${unrelated}" -eq "no" ]
 then
+	echo "Creating pedigree GRM"
 	Rscript resources/grm_relateds.R ${grmfile_all} ${grmfile_relateds} ${rel_cutoff}
 elif [ "${unrelated}" -eq "yes" ]
 then
+	echo "Removing any cryptic relateds"
 	${plink} \
 		--grm-bin ${grmfile_all} \
 		--rel-cutoff ${rel_cutoff} \
@@ -62,10 +75,6 @@ fi
 
 
 
-# Change SNP ids to chr:position:{SNP/INDEL}
-
-cp ${bfile}.bim ${bfile}.bim.original
-awk '{if (length($5) == "1" && length($6) == "1") print $1, $1":"$4":SNP", $3, $4, $5, $6;else print $1, $1":"$4":INDEL", $3, $4, $5, $6;}' <${bfile}.bim.original > ${bfile}.bim
 
 
 
@@ -74,6 +83,7 @@ awk '{if (length($5) == "1" && length($6) == "1") print $1, $1":"$4":SNP", $3, $
 # Generate meQTL format
 
 #Generate plink.raw / plink.frq #freq files are required to determine effect allele
+echo "Converting plink files to raw"
 ${plink} --bfile ${bfile} --recode A --out ${bfile} --freq
 
 #covariates file #ID COV1 COV2
@@ -101,6 +111,7 @@ ${plink} --bfile ${bfile} --recode A --out ${bfile} --freq
 #snp1 chr1 1
 #snp2 chr1 5
 
+echo "Generating matrixeqtl format genetic data"
 ./resources/make_meqtl_format.py ${bfile} ${bfile_chunksize}
 
 # This will make the files
@@ -121,4 +132,3 @@ ${plink} --bfile ${bfile} --recode A --out ${bfile} --freq
 #remove related indiv
 #check SNPs on right build
 #check allele coding, no R, ID, D anything other than ACTG
-
