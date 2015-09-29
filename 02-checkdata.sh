1662d810847c69b2290db6a96731a3a1f46dc0ba
Josine Min <epzjlm@newblue1.cm.cluster>
2015-09-29 18:17:56 +0100

    added datachecks

--- 02-checkdata.sh
+++ 02-checkdata.sh
@@ -1,11 +1,6 @@
-# include in here 
-## make sure that all geno data is there
-## summary stats
-## make sure phenotype data looks correct e.g. units and SD
-## e.g. height and bmi 
-## check covariates are there
-## etc
+R CMD BATCH /resources/datacheck/datacheck.R
 
-# Make sure that chromosome and position info is in the bim file
-# Make sure position is hg19
+grep ERROR datacheck.Rout
 
+which PLINK
+which GCTA
