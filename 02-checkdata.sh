5f01244c368d7d8755eed4634708e8de828aba72
Josine Min <epzjlm@newblue1.cm.cluster>
2015-10-05 16:31:02 +0100

    added datacheck script

--- 02-checkdata.sh
+++ 02-checkdata.sh
@@ -1,6 +1,10 @@
-R CMD BATCH /resources/datacheck/datacheck.R
+#!/bin/bash
 
-grep ERROR datacheck.Rout
+set -e
+source config
 
+R --no-save --args ${bfile_raw}.bim ${bfile_raw}.fam ${betas} ${covariates} ${provided_cellcounts} ${home_directory}/processed_data/ids/ids.txt ${home_directory}/processed_data/ids/ids_plink.txt ${home_directory}/processed_data/datacheck/no.snps.by.chr ${home_directory}/processed_data/datacheck/no.snps.by.chr.plot ${home_directory}/resources/genetics/snpsforbuildandposcheck.txt< resources/datacheck/datacheck.R > ${home_directory}/processed_data/datacheck/datacheck.Rout 2>&1
+
+which git
 which PLINK
 which GCTA
