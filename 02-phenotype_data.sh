9b0571e3dc22ba5cf91515685fe803da520cf97e
Josine Min <epzjlm@newblue1.cm.cluster>
2015-10-08 22:52:43 +0100

    add phenotype transformation

--- 02-phenotype_data.sh
+++ 02-phenotype_data.sh
@@ -2,9 +2,8 @@
 
 set -e
 source config
-
-
-
-
-
+if [ "${EWAS_phenotypes}" = "yes" ]
+then
+R --no-save --args ${EWASphenotypes} ${home_directory}/processed_data/phenotypes/EWAS.phenotypes.pdf ${covariates} ${home_directory}/processed_data/phenotypes/EWAS.phenotypes.transformed.txt 5 < resources/phenotypes/phenotype_transform.R > ${home_directory}/processed_data/phenotypes/phenotype_transform.Rout 2>&1
+fi
 # Height and BMI adjustments
