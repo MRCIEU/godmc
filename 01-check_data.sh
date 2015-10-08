7b1b62984b34d03bc08b05b66a35965f0cf7fb9d
Josine Min <epzjlm@newblue1.cm.cluster>
2015-10-08 23:12:11 +0100

    added data checks

--- 01-check_data.sh
+++ 01-check_data.sh
@@ -3,7 +3,7 @@
 set -e
 source config
 
-R --no-save --args ${bfile_raw}.bim ${bfile_raw}.fam ${betas} ${covariates} ${provided_cellcounts} ${home_directory}/processed_data/ids/ids.txt ${home_directory}/processed_data/ids/ids_plink.txt ${home_directory}/processed_data/datacheck/no.snps.by.chr ${home_directory}/processed_data/datacheck/no.snps.by.chr.plot ${home_directory}/resources/genetics/snpsforbuildandposcheck.txt< resources/datacheck/datacheck.R > ${home_directory}/processed_data/datacheck/datacheck.Rout 2>&1
+R --no-save --args ${bfile_raw}.bim ${bfile_raw}.fam ${betas} ${covariates} ${provided_cellcounts} ${home_directory}/processed_data/ids/ids.txt ${home_directory}/processed_data/ids/ids_plink.txt ${home_directory}/processed_data/datacheck/no.snps.by.chr ${home_directory}/processed_data/datacheck/no.snps.by.chr.plot ${home_directory}/resources/genetics/snpsforbuildandposcheck.txt ${EWASphenotypes} ${height} ${bmi} < resources/datacheck/datacheck.R ${cnvs} > ${home_directory}/processed_data/datacheck/datacheck.Rout 2>&1
 
 which git
 which PLINK
