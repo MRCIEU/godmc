afec11ef54fff2b7f41ea1b19ca8d394807ac9eb
Josine Min <epzjlm@newblue2.cm.cluster>
2015-10-28 16:26:12 +0000

    added logfiles

--- 02-phenotype_data.sh
+++ 02-phenotype_data.sh
@@ -2,9 +2,18 @@
 
 set -e
 source config
-
+exec &> >(tee ${phenotype_data_logfile})
 
 if [ ! "${phenotypes}" = "NULL" ]
 then
-	R --no-save --args ${intersect_ids_plink} ${phenotypes} ${ewasplot} ${covariates} ${ewastransformed} ${ewas_sd} ${ewas_plink} ${ewas_file} < resources/phenotypes/phenotype_transform.R 
+Rscript	resources/phenotypes/phenotype_transform.R \
+    ${intersect_ids_plink} \ 
+    ${phenotypes} \ 
+    ${ewasplot} \
+    ${covariates} \
+    ${ewastransformed} \
+    ${ewas_sd} \
+    ${ewas_plink} \
+    ${ewas_file} \ 
 fi
+
