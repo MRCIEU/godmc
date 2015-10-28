f6f00485959321352b7399b4764997b8d9a12cfb
Gibran Hemani <explodecomputer@gmail.com>
2015-10-28 21:50:30 +0000

    updated 04c

--- 04c-methylation_pcs.sh
+++ 04c-methylation_pcs.sh
@@ -2,12 +2,22 @@
 
 set -e
 source config
+exec &> >(tee ${methylation_pcs_logfile})
 
 # Calculate PCs for methylation, save in matrixeqtl format
 
-R --no-save --args ${methylation_rt}.RData ${meth_pc_cutoff} ${normalised_phenotypes} ${meth_pcs} < resources/methylation/methylation_pcs.R
+Rscript resources/methylation/methylation_pcs.R \
+	${methylation_rt}.RData \
+	${meth_pc_cutoff} \
+	${normalised_phenotypes} \
+	${meth_pcs}
 
 
 # Perform matrixeqtl on meth_pcs
 
-R --no-save --args ${tabfile_hm3} ${meth_pcs} ${nongenetic_meth_pcs} ${meth_pc_threshold} ${nthreads} < resources/methylation/genetic_meth_pcs.R
+Rscript resources/methylation/genetic_meth_pcs.R \
+	${tabfile_hm3} \
+	${meth_pcs} \
+	${nongenetic_meth_pcs} \
+	${meth_pc_threshold} \
+	${nthreads}
