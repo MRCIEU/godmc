e0386670e5f822bdc219f4523f4e96bc667d600d
Josine Min <epzjlm@newblue4.cm.cluster>
2015-10-30 14:44:56 +0000

    height prediction

---
+++ 02b-height_prediction.sh
@@ -0,0 +1,35 @@
+#Height prediction
+set -e
+source config
+exec &> >(tee ${height_prediction_logfile})
+
+
+if [ ! "${phenotypes}" = "NULL" ]
+then
+	echo "calculating height allele score"
+
+    ${plink} \
+	    --bfile ${bfile} \
+	    --keep ${intersect_ids_plink} \
+        --extract ${height_snps_wood}.snp \
+        --indep-pairwise 10000 5 0.1 \
+        --out ${height_snps}
+
+    fgrep -v -f ${height_snps}.prune.out ${height_snps_wood}.txt > ${height_snps_wood}.pruned
+
+    ${plink} \
+	    --bfile ${bfile} \
+	    --keep ${intersect_ids_plink} \
+	    --score ${height_snps_wood}.pruned \
+        --out ${height_snps}
+
+#var EA beta
+    Rscript ./resources/genetics/calculateheightcor.R \
+        ${height_snps}.profile \
+        ${ewastransformed} \
+        ${height_plot}
+
+    echo "Successfully completed allele score calculation "
+else
+	echo "No phenotypes"
+fi
---
+++ resources/genetics/calculateheightcor.R
@@ -0,0 +1,14 @@
+arguments <- commandArgs(T)
+score <- arguments[1]
+pheno <- arguments[2]
+heightplotfile<-arguments[3]
+
+
+r<-read.table(score,header=T,stringsAsFactors=F)
+ph<-read.table(pheno,header=T)
+m<-match(r$IID,ph$IID)
+ph<-ph[m,]
+
+pdf(heightplotfile,height=6,width=6)
+plot(r$SCORE,ph$Height,cex=0.7,main=paste0("cor=",cor.test(ph$Height,r$SCORE)$estimate))
+dev.off()
