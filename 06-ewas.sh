b257e43b837745cf2ce0d51910d04a088feb2074
Gibran Hemani <explodecomputer@gmail.com>
2015-10-30 19:09:28 +0000

    updated ewas, logging

--- 06-ewas.sh
+++ 06-ewas.sh
@@ -2,11 +2,16 @@
 
 set -e
 source config
+exec &> >(tee ${ewas_logfile})
 
 if [ "${EWAS_phenotypes}" = "NULL" ]
 then
 	echo "No phenotypes have been specified."
 else 
-	R --no-save --args ${methylation_adjusted_pcs}.RData ${ewas_transformed} ${ewas_results} < resources/methylation/ewas.R
+	Rscript resources/methylation/ewas.R \
+		${methylation_adjusted_pcs}.RData \
+		${ewastransformed} \
+		${ewas_results}
 fi
 
+echo "Successfully performed EWAS"
