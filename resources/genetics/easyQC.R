326df22891e3ecb3e403d5e5e22f1c64ef270948
Josine Min <epzjlm@newblue4.cm.cluster>
2016-01-06 17:25:40 +0000

    strand check changes

--- resources/genetics/easyQC.R
+++ resources/genetics/easyQC.R
@@ -6,30 +6,28 @@ easyQC_file <- as.character(args[4]);
 easyQC_script <- as.character(args[5]);
 
 library(data.table)
-
-bim<-read.table(paste("./",bim_file,sep=""))
+bim<-as.data.frame(fread(bim_file))
 message("read bimfile")
-frq<-read.table(paste("./",frq_file,sep=""),header=T)
+frq<-as.data.frame(fread(frq_file,header=T))
 message("read frq file")
 
 m<-match(bim[,2],frq[,2])
 bim<-data.frame(bim,frq$MAF,N=frq$NCHROBS/2,BETA=0,SE=0,PVAL=1,IMPUTATION=1,STRAND="+")
 names(bim)<-c("CHR","SNP","GeneticPOS","POS","EFFECT_ALLELE","OTHER_ALLELE","EAF","N","BETA","SE","PVAL","IMPUTATION","STRAND")
-write.table(bim[,-3],paste("./",out_file,".txt",sep=""),sep="\t",col.names=T,row.names=F,quote=F)
+write.table(bim[,-3],paste(out_file,".txt",sep=""),sep="\t",col.names=T,row.names=F,quote=F)
 message("created easyQC input file")
 
-
 library(EasyQC)
-EasyQC(paste("./",easyQC_script,sep="\t")
+EasyQC(easyQC_script)
 message("easyQC strand check finished successfully")
-
-r<-read.table(paste("./",easyQC_file,sep=""),header=T)
+message("read easyQC_file")
+r<-read.table(easyQC_file)
 message("read easyQC file")
 
 r<-r[match(bim[,2],r$SNP),]
-mm<-read.table(paste("./",out_file,".mismatch.txt",sep=""),header=T)
-notinref<-read.table(paste("./",out_file,".notinref.txt",sep=""),header=T)
-afoutlier<-read.table(paste("./",out_file,".AFCHECK.outlier.txt",sep=""),header=T)
+mm<-read.table(out_file,".mismatch.txt",header=T)
+notinref<-read.table(out_file,".notinref.txt",header=T)
+afoutlier<-read.table(out_file,".AFCHECK.outlier.txt",header=T)
 
 strand.rm<-unique(c(as.character(mm$SNP),as.character(notinref$SNP),as.character(afoutlier$SNP)))
 w<-which(bim[,2]%in%strand.rm) #4494
