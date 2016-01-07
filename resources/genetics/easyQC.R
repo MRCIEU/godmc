4200af1f77977c13df4111926b9a773ad6cff59e
Josine Min <epzjlm@newblue4.cm.cluster>
2016-01-07 18:15:38 +0000

    easyQC fix

--- resources/genetics/easyQC.R
+++ resources/genetics/easyQC.R
@@ -21,13 +21,23 @@ library(EasyQC)
 EasyQC(easyQC_script)
 message("easyQC strand check finished successfully")
 message("read easyQC_file")
-r<-read.table(easyQC_file)
-message("read easyQC file")
+r<-read.table(easyQC_file,header=T,sep="\t")
+cat(easyQC_file,"\n")
+head(easyQC_file)
+message("read easyQC file successfully")
 
 r<-r[match(bim[,2],r$SNP),]
-mm<-read.table(out_file,".mismatch.txt",header=T)
-notinref<-read.table(out_file,".notinref.txt",header=T)
-afoutlier<-read.table(out_file,".AFCHECK.outlier.txt",header=T)
+message("read mismatch file")
+mm<-read.table(paste(out_file,".mismatch.txt",sep=""),header=T)
+message("read mismatch file successfully")
+
+message("read not in ref file")
+notinref<-read.table(paste(out_file,".notinref.txt",sep=""),header=T)
+message("read not in ref file successfully")
+
+message("read AF outlier file")
+afoutlier<-read.table(paste(out_file,".AFCHECK.outlier.txt",sep=""),sep="\t",header=T)
+message("read AF outlier file successfully")
 
 strand.rm<-unique(c(as.character(mm$SNP),as.character(notinref$SNP),as.character(afoutlier$SNP)))
 w<-which(bim[,2]%in%strand.rm) #4494
@@ -37,8 +47,8 @@ a1<-a1[a1%in%w==F] #102135
 #4494
 
 SNPflip<-as.character(bim[a1,2])
-write.table(SNPflip,paste("./",out_file,".flipped.SNPs.txt",sep=""),sep="\t",quote=F,row.names=F,col.names=F)
+write.table(SNPflip,paste(out_file,".flipped.SNPs.txt",sep=""),sep="\t",quote=F,row.names=F,col.names=F)
 
 SNP.rm<-unique(c(as.character(mm$SNP),as.character(afoutlier$SNP)))
-write.table(SNP.rm,paste("./",out_file,".mismatch_afcheck.failed.SNPs.txt",sep=""),sep="\t",quote=F,row.names=F,col.names=F)
+write.table(SNP.rm,paste(out_file,".mismatch_afcheck.failed.SNPs.txt",sep=""),sep="\t",quote=F,row.names=F,col.names=F)
 message("extracted mismatched and misaligned SNPs")
