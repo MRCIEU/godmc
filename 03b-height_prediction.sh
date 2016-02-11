282d747b43e09d20c7bfb5a6058ff10f64fb4823
Gibran Hemani <explodecomputer@gmail.com>
2016-02-11 12:59:19 +0000

    updated

--- 03b-height_prediction.sh
+++ 03b-height_prediction.sh
@@ -10,28 +10,28 @@ if [ ! "${phenotypes}" = "NULL" ]
 then
 	echo "Calculating height allele score"
 
-    ${plink} \
-	    --bfile ${bfile} \
-	    --keep ${intersect_ids_plink} \
-        --extract ${height_snps_wood}.snp \
-        --indep-pairwise 10000 5 0.1 \
-        --out ${height_snps}
+	${plink} \
+		--bfile ${bfile} \
+		--keep ${intersect_ids_plink} \
+		--extract ${height_snps_wood}.snp \
+		--indep-pairwise 10000 5 0.1 \
+		--out ${height_snps}
 
-    fgrep -v -f ${height_snps}.prune.out ${height_snps_wood}.txt > ${height_snps_wood}.pruned
+	fgrep -v -f ${height_snps}.prune.out ${height_snps_wood}.txt > ${height_snps_wood}.pruned
 
-    ${plink} \
-	    --bfile ${bfile} \
-	    --keep ${intersect_ids_plink} \
-	    --score ${height_snps_wood}.pruned \
-        --out ${height_snps}
+	${plink} \
+		--bfile ${bfile} \
+		--keep ${intersect_ids_plink} \
+		--score ${height_snps_wood}.pruned \
+		--out ${height_snps}
 
-#var EA beta
-    Rscript ./resources/genetics/calculateheightcor.R \
-        ${height_snps}.profile \
-        ${ewastransformed} \
-        ${height_plot}
-    
-    echo "Successfully completed allele score calculation "
+	# var EA beta
+	Rscript ./resources/genetics/calculateheightcor.R \
+		${height_snps}.profile \
+		${ewastransformed} \
+		${height_plot}
+	
+	echo "Successfully completed allele score calculation "
 else
 	echo "No phenotypes"
 fi
