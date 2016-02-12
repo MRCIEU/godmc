7da2ac75c656850ce92e47f9bbb51e908fb35b19
Josine Min <epzjlm@node33-034.cm.cluster>
2016-02-12 13:31:04 +0000

    gzipped 1000G file

--- 01-check_data.sh
+++ 01-check_data.sh
@@ -70,7 +70,7 @@ then
 
 
 	sftp ${sftp_username}@${sftp_address}:${sftp_path}/resources <<EOF
-get 1kg_phase3_eur_allchrs_polymorphic.recoded.nodup.frq
+get 1kg_phase3_eur_allchrs_polymorphic.recoded.nodup.frq.gz
 get 1kg_phase3_eur_aut_polymorphic.recoded.nodup.frq.gz.md5sum
 EOF
 
