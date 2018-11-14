9b87da47d308d22533dfcad124bcc0545821195c
cruiz <cruiz@isgws05.isglobal.lan>
2018-11-14 21:28:59 +0100

    Solve bugs when adapting phenotypes for GWAS

--- resources/inversions/adapt_pheno.R
+++ resources/inversions/adapt_pheno.R
@@ -5,6 +5,11 @@ out <- args[3]
 
 phenodf <- read.table(pheno_file, header = TRUE)
 
-resdf <- data.frame(A = phenodf$IID, B = phenodf$IID, Height = phenodf[, "Height"])
+if ("FID" %in% colnames(phenoDF)){
+  resdf <- data.frame(A = phenodf$FID, B = phenodf$IID, Height = phenodf[, pheno])
+} else {
+  resdf <- data.frame(A = phenodf$IID, B = phenodf$IID, Height = phenodf[, pheno])
+}
+
 write.table(resdf, col.names = FALSE, row.names = FALSE, file = paste0(out, pheno, ".plink"),
             quote = FALSE)
